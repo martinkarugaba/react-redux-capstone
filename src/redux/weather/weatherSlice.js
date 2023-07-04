760d778a51ba67867b5533936dd96eaa0baf6c60
Martin Karugaba <martinkarugaba21@gmail.com>
2023-07-04 14:56:12 +0300

    Install tailwindcss

---
+++ src/redux/weather/weatherSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  data: []
+}
+
+const weatherSlice = createSlice({
+  name: second,
+  initialState,
+  reducers: {}
+});
+
+export const {} = weatherSlice.actions
+
+export default weatherSlice.reducer
