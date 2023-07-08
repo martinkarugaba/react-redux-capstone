7f59b9e76c38cd42cf1529d90c7e779c65311031
Martin Karugaba <martinkarugaba21@gmail.com>
2023-07-08 10:06:56 +0300

    Add enter icon to each country

--- src/components/SingleCountry.jsx
+++ src/components/SingleCountry.jsx
@@ -1,18 +1,20 @@
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import right from "../assets/right.png";
 
 const SingleCountry = ({ name, area }) => (
   <Link
     to={`country/${name}`}
     className="shadow border-r border-b border-slate-300 text-white bg-primary h-[170px] box-border basis-1/2 flex flex-col justify-end items-end p-4"
   >
-    <h3 className="text-xl text-right font-extrabold uppercase">{name}</h3>
+    <h3 className="text-xl text-right font-extrabold uppercase">
+      {name}
+    </h3>
     <p className="text-base font-medium">
-      {area}
-      {' '}
-      km
+      {area} km
       <sup>2</sup>
     </p>
+    <img src={right} className="h-[20px] w-[20px] mt-1" alt="country details" />
   </Link>
 );
 
